285ac5d9b7ad0b606ac5c883d55d4ec7e6a90478
田村 一馬 <tamukazu534@gmail.com>
2022-05-17 03:20:05 +0900

    追加｜完成した実装を追加

--- chrome_extension/script.js
+++ chrome_extension/script.js
@@ -1,21 +1,37 @@
-const showASIN = function() {
-  alert("ボタン押下");
-  $(".prodDetails .productDetails_db_sections tr:first").each(function () {
-    let tr = $(this);
-    let td1 = tr.children('td:first');
-    if (td1.text() == "ASIN") {
-      let td2 = tr.children('td:nth-child(2)');
-      let asin = td2.text();
-      alert(asin);
-      console.log(asin);
+const showASIN = $(function () {
+  // const category = $('#nav-subnav .nav-a-content').eq(0).text()
+  // alert('script.js実行！\nカテゴリー：' + category);
+
+  let cp = 0;
+  let prodIdx = -1;
+  const rbCaffeine = 32;
+  $('.slots-hidden').each(function (idx) {
+    const prodPattern = $(this).find('img').attr('alt');
+    const splitPattern = prodPattern.replace(/[^0-9,x]/g, '').split('x', 2);
+
+    console.log(prodPattern);
+    console.log(splitPattern);
+
+    const volume = parseInt(splitPattern[0]);
+    const quantity = parseInt(splitPattern[1]);
+
+    console.log(volume);
+    console.log(quantity);
+
+    const price = $(this).find('.twisterSwatchPrice').text().replace(/([,\￥])/g, '');
+    const newCp = Math.round((rbCaffeine * volume * quantity) / price, 1);
+
+    console.log(price);
+    console.log(newCp);
+    if (newCp > cp) {
+      prodIdx = idx;
     }
+    $(this).find('span.a-button-text').append('<div>' + newCp + ' mg/円</div>')
   });
-}
-chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: {
-      tabId: tab.id
-    },
-    function: showASIN
-  });
-});
+
+  if (prodIdx != -1) {
+    $('li.slots-hidden:eq(' + (prodIdx - 2) + ')').find('.a-button-inner').css('background-color', 'orange');
+  }
+
+  console.log(prodIdx);
+})
