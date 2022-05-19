fe74560ac0dfde6f9b5af110a9e22488fa971606
田村 一馬 <tamukazu534@gmail.com>
2022-05-19 13:52:51 +0900

    改善｜コスパの単位を分かりやすい表記(円/250ml)に変更

--- chrome_extension/script.js
+++ chrome_extension/script.js
@@ -17,8 +17,8 @@ const showBestDrink = $(function () {
       }
 
       const price = $(this).find('.twisterSwatchPrice').text().replace(/([,\￥])/g, '');
-      const newReasonability = Math.round((total / price) * 1000) / 1000;
       const newEfficiency = Math.round(((rbCaffeine * total) / price) * 1000) / 1000; // カフェイン摂取効率(mg/円)
+      const newReasonability = Math.round((price / total) * 250 * 1000) / 1000;
 
       /** カフェイン接種効率が最大のパターンのidxを保持 */
       if (newEfficiency > efficiency && newEfficiency != Infinity) {
@@ -32,7 +32,7 @@ const showBestDrink = $(function () {
 
       if (newEfficiency != (Infinity || NaN) && newReasonability != (Infinity || NaN)) {
         $(this).find('span.a-button-text').append('<div>' + newEfficiency.toPrecision(3) + ' mg/円</div>'); // 摂取効率参考値をページに表示（有効数字3ケタ）
-        $(this).find('span.a-button-text').append('<div>' + newReasonability.toPrecision(3) + ' ml/円</div>'); // コスパ参考値をページに表示（有効数字3ケタ）
+        $(this).find('span.a-button-text').append('<div>' + newReasonability.toPrecision(3) + ' 円/250ml</div>'); // コスパ参考値をページに表示（有効数字3ケタ）
       } else {
         $(this).find('span.a-button-text').append('<div>計算エラー</div>');
       }
